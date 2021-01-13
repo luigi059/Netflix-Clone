c91da33456dfcbd9306bba2ab8e701bf4ecd2705
Luigi Fernandez <luigimarkfernandez@gmail.com>
2021-01-13 20:40:38 +0000

    added eslint + prettier

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { Container, Item, Inner, Pane, Title, SubTitle, Image } from './styles/jumbotron';
 
 // compound components
-export default function Jumbotron({children,direction='row',...restProps}) {
-    return (
-        <Item direction={direction}>
-            <Inner> 
-                {children}
-            </Inner>
-        </Item>
-    )
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+  return (
+    <Item direction={direction} {...restProps}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
 }
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
-    return <Container {...restProps}>{children}</Container>;
+  return <Container {...restProps}>{children}</Container>;
 };
 Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
-    return <Pane {...restProps}>{children}</Pane>;
+  return <Pane {...restProps}>{children}</Pane>;
 };
 Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
-    return <Title {...restProps}>{children}</Title>;
+  return <Title {...restProps}>{children}</Title>;
 };
 Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
-    return <SubTitle {...restProps}>{children}</SubTitle>;
+  return <SubTitle {...restProps}>{children}</SubTitle>;
 };
 Jumbotron.Image = function JumbotronImage({ ...restProps }) {
-    return <Image {...restProps} />;
+  return <Image {...restProps} />;
 };
