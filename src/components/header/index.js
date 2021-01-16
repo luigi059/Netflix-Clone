4ff6c23760840cb3c461c846d047cd3133865902
Luigi Fernandez <luigimarkfernandez@gmail.com>
2021-01-16 20:57:47 +0000

    Header in browse page done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom';
-import { Container, Background, ButtonLink, Logo } from './styles/header';
+import {
+  Container,
+  Background,
+  ButtonLink,
+  Logo,
+  Group,
+  Link,
+  Search,
+  SearchIcon,
+  SearchInput,
+  Feature,
+  FeatureCallOut,
+  Text,
+  PlayButton,
+} from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? (
@@ -26,4 +40,48 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+  const [searchActive, setSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      {/* eslint-disable-next-line no-shadow */}
+      <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} data-testid="search-click">
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+        data-testid="search-input"
+      />
+    </Search>
+  );
+};
+
+Header.Feature = function HeaderFeature({ children }) {
+  return <Feature>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
 };
