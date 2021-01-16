36878676bdcbbccb222073381ab46d4c0c32ca26
Luigi Fernandez <luigimarkfernandez@gmail.com>
2021-01-16 15:51:26 +0000

    created sign in/up pages

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Form } from '../components';
+import HeaderContainer from '../containers/header';
+import FooterContainer from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function SignIn() {
-  return <div>Hello from signin!</div>;
+  const history = useHistory();
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+    history.push(ROUTES.BROWSE);
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+
+          <Form.Base onSubmit={handleSignin}>
+            <Form.Input placeholder="Email address" />
+            <Form.Input type="password" autoComplete="off" placeholder="Password" />
+            <Form.Submit type="submit" data-testid="sign-in">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Form } from '../components';
+import HeaderContainer from '../containers/header';
+import FooterContainer from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function SignUp() {
-  return <div>Hello from signup!</div>;
+  const history = useHistory();
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+
+    history.push(ROUTES.BROWSE);
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+
+          <Form.Base onSubmit={handleSignup}>
+            <Form.Input placeholder="First name" />
+            <Form.Input placeholder="Email address" />
+            <Form.Input type="password" autoComplete="off" placeholder="Password" />
+            <Form.Submit type="submit" data-testid="sign-up">
+              Sign Up
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            Already a user? <Form.Link to="/signin">Sign in now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
